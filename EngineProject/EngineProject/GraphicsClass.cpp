1adef4a42798f24822a7adf4b6d279e352a2b434
Jon Rurka <jon.rurka@yahoo.com>
2014-04-07 00:04:06 -0700

    Basic framework added.

---
+++ EngineProject/EngineProject/GraphicsClass.cpp
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: graphicsclass.cpp
+////////////////////////////////////////////////////////////////////////////////
+#include "GraphicsClass.h"
+
+GraphicsClass::GraphicsClass()
+{
+}
+
+GraphicsClass::GraphicsClass(const GraphicsClass& other)
+{
+}
+
+GraphicsClass::~GraphicsClass()
+{
+}
+
+bool GraphicsClass::Initialize(OpenGLClass* OpenGL, HWND hwnd)
+{
+	return true;
+}
+
+void GraphicsClass::Shutdown()
+{
+
+}
+
+bool GraphicsClass::Frame()
+{
+	return true;
+}
+
+bool GraphicsClass::Render()
+{
+	return true;
+}
---
+++ EngineProject/EngineProject/SystemClass.h
@@ -0,0 +1,65 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: systemclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _SYSTEMCLASS_H_
+#define _SYSTEMCLASS_H_
+
+///////////////////////////////
+// PRE-PROCESSING DIRECTIVES //
+///////////////////////////////
+#define WIN32_LEAN_AND_MEAN
+
+//////////////
+// INCLUDES //
+//////////////
+#include <windows.h>
+
+///////////////////////
+// MY CLASS INCLUDES //
+///////////////////////
+#include "OpenglClass.h"
+#include "InputClass.h"
+#include "GraphicsClass.h"
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: SystemClass
+////////////////////////////////////////////////////////////////////////////////
+class SystemClass
+{
+public:
+	SystemClass();
+	SystemClass(const SystemClass&);
+	~SystemClass();
+
+	bool Initialize();
+	void Shutdown();
+	void Run();
+
+	LRESULT CALLBACK MassageHandler(HWND, UINT, WPARAM, LPARAM);
+
+private:
+	bool Frame();
+	bool InitializeWindows(OpenGLClass*, int&, int&);
+	void ShutdownWindows();
+
+private:
+	LPCWSTR	m_applicationName;
+	HINSTANCE m_hinstance;
+	HWND m_hwnd;
+
+	OpenGLClass* m_OpenGL;
+	InputClass* m_Input;
+	GraphicsClass* m_Graphics;
+};
+
+/////////////////////////
+// FUNCTION PROTOTYPES //
+/////////////////////////
+static LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+/////////////
+// GLOBALS //
+/////////////
+static SystemClass* ApplicationHandle = 0;
+
+#endif
